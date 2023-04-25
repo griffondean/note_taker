7fe52744c7e967889135723b8df66d8c826b95ba
Griffon Dean <10820413@my.uvu.edu>
2023-04-24 20:06:10 -0400

    adding comments on code to describe the process

--- server.js
+++ server.js
@@ -2,80 +2,80 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const app = express();
 var PORT = 3001;
-// var PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.static(__dirname + '/public'));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//direct user to correct page depending on url
+// Directs to correct url for index page
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// Directs to correct url for notes page
 app.get("/notes", (req, res) => {
-   res.sendFile(path.join(__dirname, "./public/notes.html"))
+   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-//send json of all notes if user accesses /api/notes
+// Notes json
 app.get("/api/notes", (req, res) => {
   fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
       if (error) {
-          return console.log(error)
+          return console.log(error);
       }
-      res.json(JSON.parse(notes))
+      res.json(JSON.parse(notes));
   })
 });
 
-//use POST method to bring user input to backend
+// Adding and saving notes
 app.post("/api/notes", (req, res) => {
-    //declare const for the note currently being saved by user
-    const currentNote = req.body;
-    //retrieve notes from db.json, get id of last note, add 1 to it to create 
-    //new id, save current note with new id
+    // Declare the note that is being used by the user
+    const thisNote = req.body;
+    // Retrieve notes from db.json, get id of last note, add 1 to it to create 
+    // New id, save current note with new id
   fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
       if (error) {
-          return console.log(error)
+          return console.log(error);
       }
-      notes = JSON.parse(notes)
-      //assign unique id to each new note depending on last id.
-      //if no items in notes array, assign id as 10
+      notes = JSON.parse(notes);
+      // Assign unique id to each new note depending on last id
+      // If there are no items in the notes array, id is set to 10
       if (notes.length > 0) {
-      let lastId = notes[notes.length - 1].id
-      var id =  parseInt(lastId)+ 1
+      let lastId = notes[notes.length - 1].id;
+      var id =  parseInt(lastId) + 1;
       } else {
         var id = 10;
       }
-      //create new note object
+      // Creates new note and updates the id
       let newNote = { 
-        title: currentNote.title, 
-        text: currentNote.text, 
+        title: thisNote.title, 
+        text: thisNote.text, 
         id: id 
         }
-      //merge new note with existing notes array
-      var newNotesArr = notes.concat(newNote)
-      //write new array to db.json file and retuern it to user
-      fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(newNotesArr), (error, data) => {
+      // Combines/adds new note into existing array of notes
+      var newNotesList = notes.concat(newNote);
+
+      // Writes a new array to db.json file 
+      fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(newNotesList), (error, data) => {
         if (error) {
           return error
         }
-        console.log(newNotesArr)
-        res.json(newNotesArr);
+        console.log(newNotesList)
+        res.json(newNotesList);
       })
   });
  
 });
 
-//delete chosen note using delete http method
+// Deletes note
 app.delete("/api/notes/:id", (req, res) => {
   let deleteId = JSON.parse(req.params.id);
   console.log("ID to be deleted: " ,deleteId);
   fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
     if (error) {
-        return console.log(error)
+        return console.log(error);
     }
    let notesArray = JSON.parse(notes);
    //loop through notes array and remove note with id matching deleteId
@@ -85,9 +85,9 @@ app.delete("/api/notes/:id", (req, res) => {
 
        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(notesArray), (error, data) => {
         if (error) {
-          return error
+          return error;
         }
-        console.log(notesArray)
+        console.log(notesArray);
         res.json(notesArray);
       })
      }
@@ -96,5 +96,5 @@ app.delete("/api/notes/:id", (req, res) => {
 }); 
 });
 
-// Initialize PORT
+// Initialize PORT to run app
 app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
